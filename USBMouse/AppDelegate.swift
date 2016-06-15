28d2362e82112c40c8b8cb94670af2a588eb6ed9
Joshua May <notjosh@gmail.com>
2016-06-15 13:11:31 +1000

    Initial Commit

---
+++ USBMouse/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  USBMouse
+//
+//  Created by joshua may on 15/6/16.
+//  Copyright © 2016 joshua may. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ USBMouse/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  USBMouse
+//
+//  Created by joshua may on 15/6/16.
+//  Copyright © 2016 joshua may. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
